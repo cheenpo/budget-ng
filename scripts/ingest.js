0efdf9d6b938cd5eca93ab3c83926007090cf322
cyrus patel <cyrus@apple.com>
2016-01-02 02:31:13 -0600

    almost complete

--- scripts/ingest.js
+++ scripts/ingest.js
@@ -1,4 +1,6 @@
 var fs = require("fs");
+var parse = require('csv-parse');
+var crypto = require('crypto');
 var file = "db/budget.db";
 var exists = fs.existsSync(file);
 var ingest_file = process.argv[2];
@@ -16,17 +18,44 @@ if(!ingest_file_exists) {
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database(file);
 
-db.serialize(function() {
- //var stmt = db.prepare("INSERT OR IGNORE INTO budget VALUES (?,?,?,?,?,?,?,?,?,?)");
- fs.readFile(ingest_file, 'utf8', function (err, data) {
-  if (err) throw err;
-  var lines = data.split("\n");
-  for(var i=0; i < lines.length; i++) {
-   console.log("i: "+i+" => "+lines[i]);
-  }
- });
- //stmt.run('1', 2015, 12, 01, 40.01, 'anything', 'amex', 0, 'food', 'groceries');
- //stmt.finalize();
+fs.readFile(ingest_file, 'utf8', function (err, data) {
+ if (err) throw err;
+ var lines = data.split("\n");
+ for(var i=1; i < lines.length; i++) {
+  if(lines[i] == "") { continue; }
+  parse(lines[i], function(err, row) {
+  	//console.log(row);
+   var status = row[0][0];
+   var date = row[0][1];
+   var description = row[0][2];
+   var amount = row[0][6];
+   var account = row[0][10];
+   /*
+   var positive = true;
+   if (amount.indexOf("-") !=-1) {
+    positive = false;
+   }
+   */
+   // clean amount
+   amount = amount.replace("\"", "");
+   amount = amount.replace(",", "");
+   var amount_to_insert = amount;
+   amount = amount.replace("-", "");
+   // date details
+   var date_details = date.split("/");
+   var year = date_details[2];
+   var month = date_details[0];
+   var day = date_details[1];
+   var to_hash = (year+month+day+description+amount).replace(/ /g,"");
+   var hash = crypto.createHash('md5').update(to_hash).digest('hex');
+   //console.log(hash+" : "+to_hash);
+   var insert = false;
+   db.all("SELECT * FROM budget WHERE hash = '"+hash+"'", function(err, row) {
+   	if(row.length > 0) {
+   	 console.log("[warn] duplicate data: "+row[0]["description"]);
+   	}
+   });
+   db.run("INSERT OR IGNORE INTO budget (hash, year, month, day, amount, description, account, ignore, macro, micro) VALUES (?,?,?,?,?,?,?,?,?,?)", [hash, year, month, day, amount_to_insert, description, account, 0, 'to', 'do']);
+  });
+ }
 });
-
-db.close();
